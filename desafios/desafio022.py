018bc135c2724bc130a079761f05299e56f2b74c
Ícaro <icarosaragao2003@gmail.com>
2022-02-09 22:22:03 -0300

    Reescrevendo o arquivo desafio022.py

--- desafios/desafio022.py
+++ desafios/desafio022.py
@@ -5,6 +5,7 @@
 # > Quantas letras tem o primero nome.
 
 name = input('Informe seu nome completo: ')
+print('Analisando seu nome...')
 
 name_uppercase = name.upper()
 name_lowercase = name.lower()
@@ -13,7 +14,8 @@ join_name = ''.join(name_split)
 name_length = len(join_name)
 first_name_length = len(name_split[0])
 
-print('\nO nome escrito em maiúsculo: {}.'.format(name_uppercase))
-print('O nome escrito em minúsculo: {}.'.format(name_lowercase))
-print('{} tem {} letras.'.format(name, name_length))
-print('{} tem {} letras.'.format(name_split[0], first_name_length))
+print('Seu nome em maiúsculas é {}.'.format(name_uppercase))
+print('Seu nome em minúsculas é {}.'.format(name_lowercase))
+print('{} tem ao todo {} letras.'.format(name, name_length))
+print('Seu primeiro nome é {} e ele tem {} letras.'.format(
+    name_split[0], first_name_length))
