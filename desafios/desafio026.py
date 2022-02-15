54da5a73252c541a49d5592ec7278d076aef039c
Ícaro <icarosaragao2003@gmail.com>
2022-02-14 21:54:00 -0300

    Últimos exercícios resolvidos

--- desafios/desafio026.py
+++ desafios/desafio026.py
@@ -2,3 +2,12 @@
 # > Quantas vezes aparece a letra "A".
 # > Em que posição ela aparece a primeira vez.
 # > Em que posição ela aparece a última vez.
+
+frase = input('Informe uma frase: ')
+letra_a = frase.count('A')
+pos_inicial = frase.find('A')
+pos_final = frase.rfind('A')
+
+print('"A" aparece {} vezes na frase: {}.'.format(letra_a, frase))
+print('"A" aparece na posição {} da frase: {}.'.format(pos_inicial, frase))
+print('"A" aparece na posição {} da frase {}.'.format(pos_final, frase))
--- desafios/desafio027.py
+++ desafios/desafio027.py
@@ -3,3 +3,11 @@
 # Ex: Ana Maria de Souza
 # primeiro=Ana
 # último=Souza
+nome_completo = input('Informe seu nome completo: ')
+nome_completo_dividido = nome_completo.split()
+qtd_nome_completo = len(nome_completo_dividido)
+
+nome = nome_completo_dividido[0]
+print('Seu nome é: {}'.format(nome))
+sobrenome = nome_completo_dividido[qtd_nome_completo - 1]
+print('Seu sobrenome: {}'.format(sobrenome))
