ce195d3fbfdf29e87678a2f0a56b9e7a7d79cbb9
Ícaro <icarosaragao2003@gmail.com>
2022-02-20 16:20:02 -0300

    Alter print in 'desafio025.py'

--- desafios/desafio025.py
+++ desafios/desafio025.py
@@ -1,4 +1,4 @@
 # Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
 name = str(input('Informe seu nome completo: ')).strip()
 name_silva = 'SILVA' in name.upper()
-print('{} tem "SILVA" no nome? {}'.format(name, name_silva))
+print('{} tem Silva? {}'.format(name, name_silva))
