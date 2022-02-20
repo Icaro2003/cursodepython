8f8ebdca9a5a551333aa9fc8808df43648ad07b0
Ícaro <icarosaragao2003@gmail.com>
2022-02-20 18:20:54 -0300

    Update desafio026.py

--- desafios/desafio026.py
+++ desafios/desafio026.py
@@ -6,8 +6,8 @@
 frase = str(input('Informe uma frase: ')).strip()
 frase_upper = frase.upper()
 letra_a = frase_upper.count('A')
-pos_inicial = frase_upper.find('A')
-pos_final = frase_upper.rfind('A')
+pos_inicial = frase_upper.find('A') + 1
+pos_final = frase_upper.rfind('A') + 1
 
 print('"A" aparece {} vezes na frase: {}.'.format(letra_a, frase))
 print('"A" aparece na posição {} da frase: {}.'.format(pos_inicial, frase))
