b0cdbc8b3ff54e1e4633a649429b5ac5f4c30ad4
Ícaro <icarosaragao2003@gmail.com>
2022-02-20 16:27:38 -0300

    Commit code

--- desafios/desafio024.py
+++ desafios/desafio024.py
@@ -1,4 +1,5 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
 city = input('Informe o nome de uma cidade: ').strip() 
 city_santo = 'SANTO' in city.split()[0].upper()
+# city_santo = 'SANTO' in city[:5].upper()
 print('{} começa com "SANTO"? {}'.format(city, city_santo))
