05c03c8648a84262c82945ccaceddd8206bc9a49
Ícaro <icarosaragao2003@gmail.com>
2022-02-28 17:32:09 -0300

    Correct variable name

--- aulas/aula07a.py
+++ aulas/aula07a.py
@@ -2,14 +2,14 @@ n1 = int(input('Um valor: '))
 n2 = int(input('Outro valor: '))
 
 addition = n1 + n2
-subtration = n1 - n2
+subtraction = n1 - n2
 multiplication = n1 * n2
 division = n1 / n2
 int_division = n1 // n2
 exponentiation = n1 ** n2
 
 print('{} + {} = {}'.format(n1, n2, addition))
-print('{} - {} = {}'.format(n1, n2, subtration))
+print('{} - {} = {}'.format(n1, n2, subtraction))
 print('{} x {} = {}'.format(n1, n2, multiplication))
 print('{} / {} = {:.3f}'.format(n1, n2, division))
 print('{} // {} = {}'.format(n1, n2, int_division))
